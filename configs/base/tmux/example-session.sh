d24970f18a8ce78939b708afeb8b289ad3d8cb27
Andrew Sokolov <artas90@gmail.com>
2021-10-07 15:48:24 +0300

    Update example-session.sh

--- configs/base/tmux/example-session.sh
+++ configs/base/tmux/example-session.sh
@@ -1,16 +1,19 @@
-has_some_session() {
+no_some_session() {
   tmux has-session -t some_session 2>/dev/null || echo 1
 }
 
-if has_some_session; then
+if no_some_session; then
   tmux new-session -d -s some_session 'zsh'
+  tmux send-keys " clear" C-m
   
   tmux split-window -v
-  tmux send-keys "zsh" C-m
+  tmux send-keys " zsh" C-m
+  tmux send-keys " clear" C-m
 
   tmux select-pane -t 1
   tmux split-window -h
-  tmux send-keys "zsh" C-m
+  tmux send-keys " zsh" C-m
+  tmux send-keys " clear" C-m
 fi
 
-tmux attach-session -t some_session
+tmux attach-session -d -t some_session
