f7da597789ea38a8a34f4c78c018013285012d00
Andrew Sokolov <artas90@gmail.com>
2022-05-06 13:44:48 +0300

    update nvim config

--- configs/other/nvim/setup.sh
+++ configs/other/nvim/setup.sh
@@ -1,21 +1,7 @@
 #!/bin/sh
 
-_init=`test ! -d "$HOME/.config/nvim" && echo 1`
-
-# -- general config initial clone --
 mkdir -p $HOME/.config
-test -n "$_init" && git clone https://github.com/NvChad/NvChad ~/.config/nvim
+git clone https://github.com/artas90/nvim-config.git ~/.config/nvim
 
-# -- copy configs --
-mkdir -p $HOME/.config/nvim/lua/custom
+nvim +'hi NormalFloat guibg=#1e222a' +PackerSync
 
-_chadrc_to="$HOME/.config/nvim/lua/custom/chadrc.lua"
-cp -f "$(pwd)/chadrc.lua" $_chadrc_to
-echo "Copied to $_chadrc_to"
-
-_init_to="$HOME/.config/nvim/lua/custom/init.lua"
-cp -f "$(pwd)/init.lua" $_init_to
-echo "Copied to $_init_to"
-
-# -- initial setup --
-test -n "$_init" && nvim +'hi NormalFloat guibg=#1e222a' +PackerSync
