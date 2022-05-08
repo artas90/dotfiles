2a3ace67e908529f3ae09050f28f8e4a6cc0cec7
Andrew Sokolov <artas90@gmail.com>
2022-05-08 20:39:10 +0300

    update tmux config

--- configs/base/tmux/setup.sh
+++ configs/base/tmux/setup.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+set -euo pipefail
+
+function version {
+  echo "$1" | awk -F. '{ printf("%04d%04d%04d%04d", $1,$2,$3,$4); }'
+}
+
+tmux_ver=`tmux -V | cut -d ' ' -f 2-`
+
+if [ $(version $tmux_ver) -ge $(version "2.9") ]; then
+  from="_tmux-ge-29.conf"
+else
+  from="_tmux-lt-29.conf"  
+fi
+
 to="$HOME/.tmux.conf"
-cp -f "$(pwd)/_tmux.conf" $to
-echo "Copied to $to"
+cp -f `pwd`"/$from" $to
+echo "Copied $from to $to"
