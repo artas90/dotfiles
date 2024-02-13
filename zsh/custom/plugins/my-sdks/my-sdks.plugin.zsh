ddc6f1c5625becf74684e5eb42f0415bebdd68f5
Andrew Sokolov <artas90@gmail.com>
2024-02-14 00:25:36 +0200

    update sdks plugin

--- zsh/custom/plugins/my-sdks/my-sdks.plugin.zsh
+++ zsh/custom/plugins/my-sdks/my-sdks.plugin.zsh
@@ -21,7 +21,7 @@ fi
 
 # -- -- setup pyenv -- -- -- --
 
-if command -v pyenv &> /dev/null; then
+if command -v pyenv &> /dev/null ; then
   if [ -d "$PYENV_ROOT/bin" ]; then
     export PATH="$PYENV_ROOT/bin:$PATH"
   fi
@@ -31,7 +31,11 @@ fi
 
 # -- -- setup jdk -- -- -- --
 
+if [ -z "$JAVA_HOME" ] && [ -f "/usr/libexec/java_home" ] ; then
+  export JAVA_HOME=`/usr/libexec/java_home`
+fi
+
 _jbr="/Applications/Android Studio.app/Contents/jbr/Contents/Home"
-if [ -d "$_jbr" ]; then
+if  [ -z "$JAVA_HOME" ] && [ -d "$_jbr" ] ; then
   export JAVA_HOME="$_jbr"
 fi
