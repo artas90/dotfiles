9bbe47f474b14ae959ad31be21e53896ed21f885
Andrew Sokolov <artas90@gmail.com>
2025-01-02 16:41:12 +0200

    update git config

--- configs/base/git/setup.sh
+++ configs/base/git/setup.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
+
+email=$(git config --global --get 'user.email')
+name=$(git config --global --get 'user.name')
+
+echo_gitconfig() {
+  cat "$(pwd)/_gitconfig" \
+    | sed -e "s#__EMAIL__#${email}#" \
+    | sed -e "s#__NAME__#${name}#"
+}
+
 _git_to="$HOME/.gitconfig"
-cp -f "$(pwd)/_gitconfig" $_git_to
+echo_gitconfig > $_git_to
 echo "Created $_git_to"
 
 # copy on Windows
 _git_userprofile=$(env | grep USERPROFILE | cut -d '=' -f 2)
 if [ ! $_git_userprofile = "" ]; then
   _git_to="$_git_userprofile\\.gitconfig"
-  cp -f "$(pwd)\\_gitconfig" $_git_to
+  echo_gitconfig > $_git_to
   echo "Created $_git_to"
 fi
-
-unset _git_to
-unset _git_userprofile
