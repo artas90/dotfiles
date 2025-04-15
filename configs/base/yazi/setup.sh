7ff7b60be2366b2524a6804a0372571de1aaa808
Andrew Sokolov <artas90@gmail.com>
2025-04-16 01:55:26 +0300

    add yazi keymaps

--- configs/base/yazi/setup.sh
+++ configs/base/yazi/setup.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
-mkdir -p $HOME/.config/yazi
-
-rm -rf $HOME/.config/yazi/flavors
-git clone https://github.com/yazi-rs/flavors.git $HOME/.config/yazi/flavors
-
-_cfg_to="$HOME/.config/yazi/yazi.toml"
-cp -f "$(pwd)/yazi.toml" $_cfg_to
-echo "Created $_cfg_to"
-
-_theme_to="$HOME/.config/yazi/theme.toml"
-cp -f "$(pwd)/theme.toml" $_theme_to
-echo "Created $_theme_to"
+to="$HOME/.config/yazi"
+mkdir -p "$to"
+cp -rf "./."  "$to/."
+rm "$to/.dump.sh"
+echo "Copied configs $to"
